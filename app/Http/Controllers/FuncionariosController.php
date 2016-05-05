38d09db41efa957480d8fba48b6cfb2c45c6ac6c
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-05-05 15:28:09 -0300

    Autenticacao de logim em todas as views

--- app/Http/Controllers/FuncionariosController.php
+++ app/Http/Controllers/FuncionariosController.php
@@ -8,6 +8,11 @@ use App\Http\Requests;
 
 class FuncionariosController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('backend.veiculos.principal');
