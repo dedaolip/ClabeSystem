bf0353535781a7b3fb601a4de822666dbd4c67ae
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-05-13 07:53:37 -0300

    Populando tabela veiculos

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -55,6 +55,18 @@ $factory->define(App\Cliente::class, function(Faker\Generator $faker) {
     ];
 });
 
+$factory->define(App\Veiculo::class, function(Faker\Generator $faker) {
+    return [
+        'responsavel_id' => $faker->randomElement(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', ]),
+        'placa' => $faker->randomElement(['asd-4567', 'ghl-5487', 'sfd-5418', 'rte-6348', 'qyt-3756', 'gdo-2720', 'qbc-9675', 'pum-0745', 'lag-3854', 'fqw-4196']),
+        'ano'  => $faker->randomElement(['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2007', '2008']),
+        'registro'  => $faker->randomElement(['146745', '468567', '567856', '568978', '467456', '32567', '98678', '373456', '9867', '456745']),
+        'tipo'  => $faker->randomElement(['Onibus', 'Van', 'Carro', 'Moto']),
+    ];
+});
+
+
+
 $factory->define(App\Funcionario::class, function(Faker\Generator $faker) {
     return [
         'created_at' => $faker->dateTime,
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(CargoTableSeeder::class);
-        $this->call(ClienteTableSeeder::class);
-        $this->call(FuncionarioTableSeeder::class);
+        //$this->call(CargoTableSeeder::class);
+        //$this->call(ClienteTableSeeder::class);
+        //$this->call(FuncionarioTableSeeder::class);
+        $this->call(VeiculoTableSeeder::class);
     }
 }
