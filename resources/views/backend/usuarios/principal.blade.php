d3f0040143f305addfdd5450bb80bc20f6c1cda6
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-05-25 08:32:01 -0300

    criação da view usuarios

---
+++ resources/views/backend/usuarios/principal.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading col-lg-12">
+                        <a href="javascript:history.back(-1);" class="btn btn-default btn-sm col-lg-1" data-toggle="tooltip" data-placement="top" title="Voltar">
+                            <i class="glyphicon glyphicon-chevron-left"></i>
+                        </a>
+                        <p class="col-lg-11" style="vertical-align: middle; text-align: center; font-size: 14pt; font-weight: bolder">Usuarios</p>
+                    </div>
+
+                    <div class="panel-body">
+
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>e-mail</th>
+                                <th>Ação</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <?php foreach($users as $user): ?>
+                            <tr>
+                                <td style="vertical-align: middle"><?php echo $user->id;?></td>
+                                <td style="vertical-align: middle"><?php echo $user->name;?></td>
+                                <td style="vertical-align: middle"><?php echo $user->email;?></td>
+                                <td style="vertical-align: middle">
+                                    <a><img src="img/funcionario_edit.png" title="Editar"></a>
+                                    <a><img src="img/funcionario_delete.png" title="Deletar"></a>
+                                </td>
+                            </tr>
+                            <?php endforeach;?>
+                            </tbody>
+
+                        </table>
+
+                        <div class="col-lg-10 text-center">
+                            {!! $users->render() !!}
+                        </div>
+
+                        <div class="col-lg-10">
+                            <a class="col-lg-10"><img src="img/funcionario_add.png" title="Novo"><p style="text-align: center; color: black"></p></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
