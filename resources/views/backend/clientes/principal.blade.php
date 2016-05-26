e9d8da20496cdb12cc12aa8b2a0829f14cc7a8a4
Felipe Novaes Medeiros <felipenovaesmedeiros@gmail.com>
2016-05-25 23:39:41 -0300

    updt clientes.principal

--- resources/views/backend/clientes/principal.blade.php
+++ resources/views/backend/clientes/principal.blade.php
@@ -13,37 +13,50 @@
                 </div>
 
                 <div class="panel-body">
-
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Contato</th>
-                            <th>Ação</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        <?php foreach($clientes as $cliente): ?>
-                        <tr>
-                            <td style="vertical-align: middle"><?php echo $cliente->id;?></td>
-                            <td style="vertical-align: middle"><?php echo $cliente->pessoa->nome;?></td>
-                            <td style="vertical-align: middle"><?php echo $cliente->pessoa->fone;?></td>
-                            <td style="vertical-align: middle">
-                                <a href="{!! route('cli.edit', ['id' => $cliente->id]) !!}"><img src="<?= asset('img/funcionario_edit.png'); ?>" title="Editar"></a>
-                                <a href="{!! route('cli.destroy', ['id' => $cliente->id]) !!}"><img src="<?= asset('img/funcionario_delete.png'); ?>" title="Deletar"></a>
-                            </td>
-                        </tr>
-                        <?php endforeach;?>
-                        </tbody>
-
-                    </table>
-
-                    <div class="col-lg-10 text-center">
-                    {!! $clientes->render() !!}
+                    <div class="form-group col-lg-12">
+                        <label class="control-label text-center" style="font-weight: bold; font-size: 20pt">Pesquisar</label>
+                    </div>
+                    <div class="form-group col-lg-1">
+                        <label for="pesquisar" class="control-label" style="padding-top: 5pt">Nome:</label>
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <input class="form-control" type="text" style="padding: 5pt">
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <button class="btn btnPesquisa" id="btnPesquisar">Pesquisar</button>
                     </div>
 
+                    <div class="col-lg-12 divTabela" id="divTabela">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Contato</th>
+                                <th>Ação</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <?php foreach($clientes as $cliente): ?>
+                            <tr>
+                                <td style="vertical-align: middle"><?php echo $cliente->id;?></td>
+                                <td style="vertical-align: middle"><?php echo $cliente->pessoa->nome;?></td>
+                                <td style="vertical-align: middle"><?php echo $cliente->pessoa->fone;?></td>
+                                <td style="vertical-align: middle">
+                                    <a href="{!! route('cli.edit', ['id' => $cliente->id]) !!}"><img src="<?= asset('img/funcionario_edit.png'); ?>" title="Editar"></a>
+                                    <a href="{!! route('cli.destroy', ['id' => $cliente->id]) !!}"><img src="<?= asset('img/funcionario_delete.png'); ?>" title="Deletar"></a>
+                                </td>
+                            </tr>
+                            <?php endforeach;?>
+                            </tbody>
+
+                        </table>
+
+                        <div class="col-lg-10 text-center">
+                        {!! $clientes->render() !!}
+                        </div>
+                    </div>
                     <div class="col-lg-10">
                         <a class="col-lg-10" href="{!! route('cli.create') !!}"><img src="<?= asset('img/funcionario_add.png'); ?>" title="Novo"><p style="text-align: center; color: black"></p></a>
                     </div>
@@ -52,5 +65,6 @@
             </div>
         </div>
     </div>
+    
 </div>
 @endsection
